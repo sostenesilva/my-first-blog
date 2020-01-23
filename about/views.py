0d5c8137b2a0dd17500045f0ec74272faf901be5
sostenes <sostenesilvaa@gmail.com>
2020-01-23 15:31:50 -0300

    Adicionados os posts da pagina hobby

--- about/views.py
+++ about/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def pagAbout(request):
-    return render(request,'pagAbout.html')
+    return render(request,'pagAbout.html',{'posts': posts})
