cf142aa3efab323cf553cecff70ffc9acf573c4b
sostenes <sostenesilvaa@gmail.com>
2020-01-23 19:09:38 -0300

    Desenvolvendo um primeiro css

--- about/views.py
+++ about/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def pagAbout(request):
-    return render(request,'pagAbout.html',{'posts': posts})
+    return render(request,'pagAbout.html',{})
