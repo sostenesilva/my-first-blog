b2a2f5a1bc9ebbdfdb4be10ff27caf1ceb1d4091
sostenes <sostenesilvaa@gmail.com>
2020-02-07 15:53:57 -0300

    paginator

--- hobby/views.py
+++ hobby/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+from django.core.paginator import Paginator
 # Create your views here.
 
 def pagPrincipal(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts_list = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(posts_list, 9)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
     return render(request,'principal/pagPrincipal.html',{'posts': posts})
 
 def post_detail(request, pk):
